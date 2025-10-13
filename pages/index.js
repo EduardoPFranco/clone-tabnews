a25e7007e4368c36abca1d6bc6f4dd9aad3e65fb
Eduardo Pinheiro Franco <eduardo.whiteness@gmail.com>
2025-10-13 22:36:58 +0000

    Mudando a frase novamente

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Sistema Tab News desenvolvido seguindo o curso.dev</h1>        
+    return <h1>Raissa Cardoso, minha princesa linda!</h1>        
 }
 
 export default Home;
