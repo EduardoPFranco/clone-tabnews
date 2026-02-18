bf40a7c87954076c9a78bffe3d3e25a37ec89fe2
Eduardo Pinheiro Franco <eduardo.whiteness@gmail.com>
2026-02-18 21:11:18 +0000

    adds `/app/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,13 +1,39 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
-    databaseURL: process.env.DATABASE_URL,
+  const dbClinet = await database.getNewClient();
+
+  const defaultMigrationsOptions = {
+    dbClient: dbClinet,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
-  });
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
 
-  response.status(200).json([migrations]);
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClinet.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+
+    await dbClinet.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
 }
